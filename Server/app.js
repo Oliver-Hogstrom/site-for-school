4a322d01f7fef0ad207df34e51cb866eb73db4ab
Oliver Högström <oliver.hogstrom@elev.ga.ntig.se>
2020-12-02 11:50:02 +0100

    Init

--- Server/app.js
+++ Server/app.js
@@ -3,22 +3,23 @@ const app = express()
 const ejs = require ('ejs')
 const port = 3000
 
-const clientDir = __dirname + `\\client\\`
-const htmlDir = __dirname + `\\HTML\\`
-const styleDir = __dirname + `\\SCSS\\`
-app.use(express.static(styleDir))
-app.use(express.static(clientDir))
-
 app.set('view-engine', 'ejs')
+
+const htmlDir = __dirname + `\\html\\`
+const styleDir = __dirname + `\\style\\`
 
 // All the HTML files here
 app.get('/', (req, res) => {
   res.sendFile(htmlDir + 'index.html')
 })
 
-// All the Style files here
-app.get('/bs', (req, res) => {
-  res.sendFile('../bootstrap/bootstrap.min.css')
+// Some style for the site aka bootstrap
+app.get('/bootstrap', (req, res) => {
+  res.sendFile(styleDir + 'bootstrap.min.css')
+})
+
+app.get('/css', (req, res) => {
+  res.sendFile(styleDir + 'main.css')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
