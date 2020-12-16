67cc4a92ad4a8a4791df99d7c98164a42d16b923
oliverhogstrom <54627509+oliverhogstrom@users.noreply.github.com>
2020-12-16 11:57:02 +0100

    Subside done
    We have created the subsides that Thomas wanted and now waiting for me to host the site on ntkronhus.tk

--- Server/app.js
+++ Server/app.js
@@ -7,10 +7,22 @@ app.set('view-engine', 'ejs')
 
 const htmlDir = __dirname + `\\html\\`
 const styleDir = __dirname + `\\style\\`
+const clientDir = __dirname + `\\client\\`
 
 // All the HTML files here
 app.get('/', (req, res) => {
   res.sendFile(htmlDir + 'index.html')
+})
+
+app.get('/Utbildning', (req, res) => {
+  res.sendFile(htmlDir + 'utbildning.html')
+})
+app.get('/Kontakt', (req, res) => {
+  res.sendFile(htmlDir + 'kontakt.html')
+})
+
+app.get('/Support', (req, res) => {
+  res.sendFile(htmlDir + 'support.html')
 })
 
 // Some style for the site aka bootstrap
@@ -21,5 +33,8 @@ app.get('/bootstrap', (req, res) => {
 app.get('/css', (req, res) => {
   res.sendFile(styleDir + 'main.css')
 })
+
+// All the pictures
+app.use(express.static(clientDir))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
